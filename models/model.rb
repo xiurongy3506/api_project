460d9021591d2a09b109cd12b9461ca70d33a340
Xiurong Yu <xiurongy3506@hstat.org>
2019-03-01 17:27:25 +0000

    change link

--- models/model.rb
+++ models/model.rb
@@ -2,18 +2,18 @@ require 'net/http'
 require 'json'
 require 'pp'
 
-# url = 'https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/images/'
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# # pp JSON.parse(response)
-# result = JSON.parse(response)
-# puts result["photos"]
+url = 'https://api.teleport.org/api/cities/?search=san%20francisco'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+# pp JSON.parse(response)
+result = JSON.parse(response)
+pp result
 
-def get_info(city_name)
-    url = 'https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/images/'
-    url = URI(url)
-    response = Net::HTTP.get(uri)
-    result = JSON.parse(response)
-    result["photos"]
-end
+# def get_info(city_name)
+#     url = 'https://api.teleport.org/api/cities/?search=san%20francisco'
+#     url = URI(url)
+#     response = Net::HTTP.get(uri)
+#     result = JSON.parse(response)
+#     # result["photos"]
+# end
 
