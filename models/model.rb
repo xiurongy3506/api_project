80b54482713a7839dd00bceb146e514d9c2d3e95
Xiurong Yu <xiurongy3506@hstat.org>
2019-03-04 05:12:23 +0000

    test out city api link

--- models/model.rb
+++ models/model.rb
@@ -8,27 +8,35 @@ require 'pp'
 # result = JSON.parse(response)
 # pp result
 
+
+# url = 'https://api.teleport.org/api/cities/?search=new york'
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
+# # pp JSON.parse(response)
+# result = JSON.parse(response)
+# # url is in str form
+# pp result["_embedded"]["city:search-results"][0]["_links"]["city:item"]["href"]
+
 # url = 'https://api.teleport.org/api/urban_areas/slug:new-york/images/'
 # uri = URI(url)
 # response = Net::HTTP.get(uri)
 # # pp JSON.parse(response)
 # result = JSON.parse(response)
 # # url is in str form
-# pp result
-# # ["photos"][0]["image"]["web"]
+# pp result ["photos"][0]["image"]["web"]
 
 
     
-# def get_info(city_name)
-#     url = 'https://api.teleport.org/api/cities/?search=Boston, New York, United States'
+# def get_population_link
+#     url = 'https://api.teleport.org/api/cities/?search=Boston'
 #     uri = URI(url)
 #     response = Net::HTTP.get(uri)
 #     result = JSON.parse(response)
-#     result
+#     pp result["_embedded"]["city:search-results"][0]["_links"]["city:item"]["href"]
 # end
 
 def get_img(city_img)
-    url = 'https://api.teleport.org/api/urban_areas/slug:new-york/images/'
+    url = 'https://api.teleport.org/api/urban_areas/slug:' + city_img + '/images/'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     result = JSON.parse(response)
