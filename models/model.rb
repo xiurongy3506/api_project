39aa42ccc1ef37ad82ce11d1ca739bc46f17da51
Xiurong Yu <xiurongy3506@hstat.org>
2019-03-06 13:32:30 +0000

    add simple design to index and adds recreation api

--- models/model.rb
+++ models/model.rb
@@ -8,40 +8,24 @@ require 'pp'
 # result = JSON.parse(response)
 # pp result
 
-
-# url = 'https://api.teleport.org/api/cities/?search=boston'
+# url = 'https://ridb.recreation.gov/api/activities'
 # uri = URI(url)
 # response = Net::HTTP.get(uri)
-# # pp JSON.parse(response)
 # result = JSON.parse(response)
-# # url is in str form
-# pp result["_embedded"]["city:search-results"][0]["_links"]["city:item"]["href"]
-
-# url = get_population_link
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# # pp JSON.parse(response)
-# result = JSON.parse(response)
-# # url is in str form
 # pp result
+# value = ENV['API_KEY'], ENV['PASSWORD']
+# value.authorization.access_token = '123'
+# https://ridb.recreation.gov/docs#/Activities/getActivities
 
-# url = 'https://api.teleport.org/api/urban_areas/slug:new-york/images/'
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# # pp JSON.parse(response)
-# result = JSON.parse(response)
-# # url is in str form
-# pp result ["photos"][0]["image"]["web"]
-
-def get_population_link
-    url = 'https://api.teleport.org/api/cities/?search=boston'
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    # pp JSON.parse(response)
-    result = JSON.parse(response)
-    # url is in str form
-    result["_embedded"]["city:search-results"][0]["_links"]["city:item"]["href"]
-end
+# def get_population_link
+#     url = 'https://api.teleport.org/api/cities/?search=boston'
+#     uri = URI(url)
+#     response = Net::HTTP.get(uri)
+#     # pp JSON.parse(response)
+#     result = JSON.parse(response)
+#     # url is in str form
+#     result["_embedded"]["city:search-results"][0]["_links"]["city:item"]["href"]
+# end
 
 def get_population_data
     url = get_population_link
